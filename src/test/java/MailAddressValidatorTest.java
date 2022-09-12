d6bcf099bb02053efad89262b7d254a77467e74b
flmeyer <florian.meyer@capgemini.com>
2022-09-12 13:59:59 +0200

    AAA

--- src/test/java/MailAddressValidatorTest.java
+++ src/test/java/MailAddressValidatorTest.java
@@ -1,10 +1,19 @@
 import com.example.MailAddressValidator;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MailAddressValidatorTest {
     @Test
     public void test() {
+        // Arrange
         MailAddressValidator validator = new MailAddressValidator();
-        
+        String address = "florian.meyer@capgemini.com";
+
+        // Act
+        boolean result = validator.isValidEmailAddress(address);
+
+        // Assert
+        assertTrue(result);
     }
 }
